1486cd886986f32f0b7b23188f030ec2a4db2ecd
Trevor Konty <trevkonty@gmail.com>
2022-04-18 12:57:41 -0400

    Added an EnemyManager object. Added a rotation to the enemies entry. Changed background color and added walls.

---
+++ Galaga Final/Assets/Scripts/EnemyBehavior.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7569c972c3bf60c5d42a5f2050f8ac07727a521bec6d32433e4e9623ed97b215
+size 1169
--- Galaga Final/Assets/Scripts/EnemyEntry.cs
+++ Galaga Final/Assets/Scripts/EnemyEntry.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:73e53e33f33b2a066dee884fbb0fee6c623a89c3da1472fdab291cd748fa8a1a
-size 2375
+oid sha256:2cf190496043664e07e2fefc9aa65d77a779adf611f53995288858bd90917c3f
+size 3000
