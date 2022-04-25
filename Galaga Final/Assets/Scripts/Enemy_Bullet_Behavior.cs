aa46a2223b8a73adfbb95d6807848917663b8ac6
Highway-Star <lrh8760@nyu.edu>
2022-04-25 13:41:32 -0400

    Added an enemy bullet (unfinished)

---
+++ Galaga Final/Assets/Scripts/Enemy_Bullet_Behavior.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:4fa1576d4c7db158b777c91980a40e4faae430f238cc5d2dd4c86887dec77a7c
+size 1344
--- Galaga Final/Assets/Scripts/Life_Manager.cs
+++ Galaga Final/Assets/Scripts/Life_Manager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b8beb3c59ce8d4aee5d916a26630510d5035a66215935acb02b235bbe8297f7a
-size 1086
+oid sha256:c0b84f0b2af588fb9036d78b40e503946826253384d3ac3e81f55a065f1edc4b
+size 1088
