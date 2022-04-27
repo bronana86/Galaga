5430d982a3809b6ed0b67714aa2c4dc59cab0f1e
Trevor Konty <trevkonty@gmail.com>
2022-04-27 13:08:13 -0400

    Fixed merge errors between Jason and Trevor.

--- Galaga Final/Assets/Scripts/Enemy_Score.cs
+++ Galaga Final/Assets/Scripts/Enemy_Score.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0a0c3d44cc5fc0dafaea7fb1938dbaccdac1795ef80fda2013e2a785c980331f
-size 1342
+oid sha256:8bfada9e66ce49582dca0c01efbf676f0eedafa09d35184a72b1ddac988882a4
+size 1535
