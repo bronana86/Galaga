9c4f14ec5307597f4bb797a839447121f6656e9b
Highway-Star <lrh8760@nyu.edu>
2022-05-02 12:02:12 -0400

    Added a return to title screen after game over after a few seconds.

--- Galaga Final/Assets/Scripts/Text_Manager.cs
+++ Galaga Final/Assets/Scripts/Text_Manager.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8b2750716d77c8c0d54267a9d951a240af7e55f4e9afde225c503184f1af5411
-size 904
+oid sha256:1d98048a6d9a84f91c065cc89d3006fd95d1e5e31788aa648ade4bd37bc88a2c
+size 1080
