24665ab0dff976404447cdf5a9f2b72fba0dbbbd
JasonChiu-git <cc6791@nyu.edu>
2022-05-02 13:37:57 -0400

    death animation

--- Galaga Final/Assets/Scripts/Player_Death.cs
+++ Galaga Final/Assets/Scripts/Player_Death.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a79ef013f7fa630feb184ae0bf15c5186fe77c52c4feaabb6d3b0397c06dc1b6
-size 1942
+oid sha256:5c4c4286eb4c7cafa1cf7b62780fa6de349e59b1511f861b1e1ebfb9bf52e93a
+size 2127
