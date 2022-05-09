a9914c64a4f76318abd0af3a8a194c79a57d42a9
Trevor Konty <trevkonty@gmail.com>
2022-05-09 13:57:41 -0400

    Monday Playtest.

--- Galaga Final/Assets/Scripts/Enemy_Score.cs
+++ Galaga Final/Assets/Scripts/Enemy_Score.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:0ce7f9430704eb464ea3b170018fe592533da2672b5186bef452a7f3c0bd9644
-size 2168
+oid sha256:dd4837acf50a000b5b67ceb27e46a37f1526279619dcd400b4d81057029dd94a
+size 2182
