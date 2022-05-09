376d38eaca0a8e6ae0d40eff7edcc9b2c18ffac1
Trevor Konty <trevkonty@gmail.com>
2022-05-09 11:30:26 -0400

    Fixed a bug where enemies would stack after spawning.

--- Galaga Final/Assets/Scripts/Enemy_Score.cs
+++ Galaga Final/Assets/Scripts/Enemy_Score.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7a0e08f5235eb780f8cae36bebc334869a9510b5f4de0b3ac83158b4378f68e4
-size 1920
+oid sha256:9ce68d2ba60643e399b29cf708a6d44c6411b3587799e2a12fa8553482479a6e
+size 1910
