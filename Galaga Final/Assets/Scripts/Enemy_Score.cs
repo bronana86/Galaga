4272a00e57e58fe722aaa2253700ba2e129f2f6e
JasonChiu-git <cc6791@nyu.edu>
2022-05-09 11:13:45 -0400

    hurtanimation for boss and boss health

--- Galaga Final/Assets/Scripts/Enemy_Score.cs
+++ Galaga Final/Assets/Scripts/Enemy_Score.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e6c1c3c5362315ceb70b313dd808ad7593270cdb6fa8b005f0e1c406b41b74e0
-size 1929
+oid sha256:7a0e08f5235eb780f8cae36bebc334869a9510b5f4de0b3ac83158b4378f68e4
+size 1920
